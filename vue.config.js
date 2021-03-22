971c14921737a6b5873314ea5a68930adf54d5f7
a869057476 <869057476@qq.com>
2021-03-22 21:51:22 +0800

    修改静态资源路径

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: huangwei
+ * @Date: 2021-03-22 21:45:03
+ * @LastEditors: huangwei
+ * @LastEditTime: 2021-03-22 21:50:08
+ */
+'use strict'
+module.exports = {
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+}
